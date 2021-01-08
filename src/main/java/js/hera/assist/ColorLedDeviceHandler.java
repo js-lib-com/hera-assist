e8769df531f99bb39b9c30210df373322bb3fc9c
Iulian Rotaru <iulian@gnotis.ro>
2021-01-08 13:41:36 +0200

    Add brightness and on / off to ColorLED.

--- src/main/java/js/hera/assist/ColorLedDeviceHandler.java
+++ src/main/java/js/hera/assist/ColorLedDeviceHandler.java
@@ -26,6 +26,14 @@ public class ColorLedDeviceHandler extends DeviceHandler
       int colorRGB = (int)colorParameters.get("spectrumRGB");
       return states(rmi(user, "setColor", Integer.toString(colorRGB), int.class));
 
+    case "action.devices.commands.BrightnessAbsolute":
+      double brightness = (int)parameters.get("brightness") / 100.0;
+      return states(rmi(user, "setBrightness", Double.toString(brightness), double.class));
+
+    case "action.devices.commands.OnOff":
+      boolean state = (boolean)parameters.get("on");
+      return states(rmi(user, "setState", state ? "1.0" : "0.0", boolean.class));
+
     default:
       log.warn("Command |%s| not implemented.", command);
       break;
@@ -45,6 +53,20 @@ public class ColorLedDeviceHandler extends DeviceHandler
 
     Map<String, Object> states = new HashMap<>();
     states.put("color", color);
+    return states;
+  }
+
+  private static Map<String, Object> states(double value)
+  {
+    Map<String, Object> states = new HashMap<>();
+    states.put("brightness", Math.round(100 * value));
+    return states;
+  }
+
+  private static Map<String, Object> states(boolean value)
+  {
+    Map<String, Object> states = new HashMap<>();
+    states.put("on", value);
     return states;
   }
 }
