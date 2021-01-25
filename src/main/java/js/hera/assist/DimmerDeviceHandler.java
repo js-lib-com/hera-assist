b472ee6ebb74575cc7259e16a2a67d822bae8593
Iulian Rotaru <iulian@gnotis.ro>
2021-01-25 22:15:50 +0200

    Add light dimmer to assistant.

---
+++ src/main/java/js/hera/assist/DimmerDeviceHandler.java
@@ -0,0 +1,45 @@
+package js.hera.assist;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import js.log.Log;
+import js.log.LogFactory;
+
+public class DimmerDeviceHandler extends DeviceHandler
+{
+  private static final Log log = LogFactory.getLog(DimmerDeviceHandler.class);
+
+  public DimmerDeviceHandler()
+  {
+    log.trace("DimmerDeviceHandler()");
+  }
+
+  public Map<String, Object> execute(String user, String command, Map<String, Object> parameters)
+  {
+    switch(command) {
+    case "action.devices.commands.BrightnessAbsolute":
+      Integer brightness = (Integer)parameters.get("brightness");
+      assert brightness < 256;
+      byte value = (byte)Math.round(255.0 * brightness.doubleValue() / 100.0);
+      return states(rmi(user, "updateValue", Byte.toString(value), byte.class));
+
+    default:
+      log.warn("Command |%s| not implemented.", command);
+      break;
+    }
+    return null;
+  }
+
+  public Map<String, Object> query(String user)
+  {
+    return states(rmi(user, "getValue", byte.class));
+  }
+
+  private static Map<String, Object> states(byte value)
+  {
+    Map<String, Object> states = new HashMap<>();
+    states.put("brightness", value);
+    return states;
+  }
+}
