7509f9df9aef4464ccb402ab7ea15d8a06adc03d
Iulian Rotaru <iulian@gnotis.ro>
2021-01-26 12:59:16 +0200

    Add on / off control to light dimmer.

--- src/main/java/js/hera/assist/DimmerDeviceHandler.java
+++ src/main/java/js/hera/assist/DimmerDeviceHandler.java
@@ -18,11 +18,15 @@ public class DimmerDeviceHandler extends DeviceHandler
   public Map<String, Object> execute(String user, String command, Map<String, Object> parameters)
   {
     switch(command) {
+    case "action.devices.commands.OnOff":
+      boolean parameter = (boolean)parameters.get("on");
+      return states(rmi(user, "setState", parameter ? "1.0" : "0.0", State.class));
+      
     case "action.devices.commands.BrightnessAbsolute":
       Integer brightness = (Integer)parameters.get("brightness");
       assert brightness <= 100;
-      Long value = Math.round(255.0 * brightness.doubleValue() / 100.0);
-      return states(rmi(user, "updateValue", value.toString(), byte.class));
+      int value = (int)Math.round(255.0 * brightness.doubleValue() / 100.0);
+      return states(rmi(user, "setValue", Integer.toString(value), State.class));
 
     default:
       log.warn("Command |%s| not implemented.", command);
@@ -33,13 +37,27 @@ public class DimmerDeviceHandler extends DeviceHandler
 
   public Map<String, Object> query(String user)
   {
-    return states(rmi(user, "getValue", byte.class));
+    return states(rmi(user, "getState", State.class));
   }
 
-  private static Map<String, Object> states(byte value)
+  private static Map<String, Object> states(State state)
   {
     Map<String, Object> states = new HashMap<>();
-    states.put("brightness", value);
+    if(state == null) {
+      states.put("online", false);
+      return states;
+    }
+
+    states.put("online", true);
+    states.put("on", state.running);
+    assert state.value < 256;
+    states.put("brightness", (int)Math.round(100.0 * state.value.doubleValue() / 256.0));
     return states;
+  }
+
+  private static class State
+  {
+    boolean running;
+    Integer value;
   }
 }
